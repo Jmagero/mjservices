777ddf9f6849348b865c3f3971d105d42f5a29bf
Jocyline Magero <jmagero54@gmail.com>
2023-07-01 13:41:25 -0500

    Add notification service

--- notification/src/main/java/com/jm/notification/NotificationController.java
+++ notification/src/main/java/com/jm/notification/NotificationController.java
@@ -1,16 +1,19 @@
 package com.jm.notification;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/notification")
 @AllArgsConstructor
+@Slf4j
 public class NotificationController {
 
     final  private NotificationService notificationService;
     @PostMapping
     public void sendNotification(@RequestBody NotificationRequest notificationRequest){
+        log.info("New notification ...{}", notificationRequest);
         notificationService.send(notificationRequest);
     }
 }
